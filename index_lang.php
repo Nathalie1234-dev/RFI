2cf96e9073a75edcfc3204f9e7292a23f894d15c
Nathalie1234-dev <164439153+Nathalie1234-dev@users.noreply.github.com>
2024-04-16 11:29:42 +0300

    Update index_lang.php

--- index_lang.php
+++ index_lang.php
@@ -1,6 +1,16 @@
 <?php
-$c = "dir";
-system($c);
-echo ('ok');
+
+$chemin_courant='./';
+
+//utilise glob() pour obtenir la liste des fichier et 
+$element=glob($chemin_courant.'*',GLOB_MARK);
+
+//affiche les élements du répertoire courant
+foreach($element as $element){
+    echo basename($element) . "<br>";
+}
 
 ?>
+
+ <!-- flag.php   -->
+
